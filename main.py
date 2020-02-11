cf3605273e7e0ff64deed737a76a224a652eaeef
eder <eder@MacBook-Pro-de-eder.local>
2020-02-11 16:54:16 +0100

    write in main

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+choice = ""
+if __name__=='__main__':
+    while choice != "q":
+        choice = input("enter your choice \n "
+                       "(a) for manage confereces "
+                       "\n (b) for manange speakers "
+                       "\n (q) for exit :")
+        if choice == "a":
+            pass
+
+        if choice == "b":
+            pass
+
+
+        if choice == "q":
+            exit()
